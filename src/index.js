2adb95f15bbcb9246f6b8b170813db5696fb9b01
ArturStahov <vtemy.at.ua@gmail.com>
2020-12-16 09:54:38 +0200

    v1.0

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import '@csstools/normalize.css';
+import './base.css'
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.querySelector('#root')
+);
+
