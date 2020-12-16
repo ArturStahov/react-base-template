25e64d088b18156c1dfffc6581e6a6311c6e2206
ArturStahov <vtemy.at.ua@gmail.com>
2020-12-16 09:57:50 +0200

    add app.js

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import '@csstools/normalize.css';
 import './base.css'
+import App from './App'
 
 ReactDOM.render(
   <React.StrictMode>
